8205380be7e5acbbcd18788dcf4262ec16990a0b
QCRI Admin <tgamal@qf.org.qa>
2015-06-25 07:56:26 +0300

    models modified

--- V3VServer/coreserver/models.py
+++ V3VServer/coreserver/models.py
@@ -15,13 +15,13 @@ class Instance(models.Model):
     dns = models.CharField(max_length=255)
     username=models.CharField(max_length=255)
     password=models.CharField(max_length=255)
-    port=models.IntegerField()
+    port=models.IntegerField(default=22)
     ssh_key=models.CharField(max_length=255)
-    number_of_cpus = 0
-    number_of_cores_percpu = 0
-    aggregate_memory = 0
-    speed_per_core = 0
-    price = 0
+    number_of_cpus = models.IntegerField(default=0)
+    number_of_cores_percpu = models.IntegerField(default=0)
+    aggregate_memory = models.IntegerField(default=0)
+    speed_per_core = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     status = models.CharField(max_length=255) #running, stopped, terminated
     cloud_provider = models.ForeignKey(CloudProvider)
     class Meta:
@@ -40,13 +40,13 @@ class Segment2D(models.Model):
     id=models.AutoField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True, null=False)
     name=models.CharField(max_length=255, null=False)
-    category=models.CharField(max_length=255)
-    duration=models.IntegerField()
-    frame_rate=models.IntegerField()
+    category=models.CharField(max_length=255, null=True)
+    duration=models.IntegerField(null=True)
+    frame_rate=models.IntegerField(null=True)
     location=models.CharField(max_length=255)
     resolution=models.CharField(max_length=255)
     url=models.CharField(max_length=255)
-    upload_date=models.DateTimeField()
+    upload_date=models.DateTimeField(null=True)
     # Relationships
     account=models.ForeignKey(Account)
     instance=ForeignKey(Instance)
--- V3VServer/coreserver/tests.py
+++ V3VServer/coreserver/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+#from coreserver.models import Segment2D,Account,Instance,CloudProvider
+#import datetime
 # Create your tests here.
+#if __name__ == '__main__':
+    #cloud=CloudProvider.objects.create(name="local")
+    #cloud=CloudProvider.objects.get(name="local")
+    #instance = Instance.objects.create(ipaddress="127.0.0.1", username="qcriadmin", password="123456", status="RUNNING", cloud_provider=cloud)
+    #account = Account.objects.get(pk=1)
+    #Segment2D.objects.create(name="segment_0001", account=account, instance=instance)
