b8771ff975ddbaa2653c8eed4ed0c0682dddc0b1
Hazem Samir <hazemhazem.1995@gmail.com>
2016-02-04 13:51:04 +0300

    added new method to send email to a list of team emails

--- V3VServer/coreserver/utils/taskinfoemailsender.py
+++ V3VServer/coreserver/utils/taskinfoemailsender.py
@@ -7,6 +7,7 @@ Created on Feb 4, 2016
 from coreserver.utils.emailsender import EmailSender
 from coreserver.models import Email, Segment2D
 from matplotlib.testing.jpl_units.Duration import Duration
+from V3VServer.settings import v3vteam_emails
 
 class EmailsTemplates(object):
     
@@ -23,10 +24,18 @@ thanks for using our service,
         
 V3V team
 """
-    
+    debug_message_header = """
+this message is sent to V3V team email list concerning task assigned by %(receiver)s
+
+--------------------------------
+%(text_msg)s
+"""
     
     @classmethod
     def send_task_info_email(cls, segment2D):
+        '''
+            sends email with uploaded video information
+        '''
         sender = Email.objects.get(active=1)
         subject = "V3V: video uploaded"
         sender_address = sender.address
@@ -38,5 +47,15 @@ V3V team
         text_msg = cls.task_info_email_template % data
         if receiver:
             EmailSender.send_email(text_msg, sender_address,sender_password, receiver, subject)
+            cls.send_email_to_team(text_msg, sender_address, sender_password, receiver, subject)
+
+    @classmethod            
+    def send_email_to_team(cls, text_msg, sender_address,sender_password, receiver, subject, with_header=True):
+        if with_header:
+            text_msg = cls.debug_message_header % {'receiver':receiver, 'text_msg':text_msg}
+        if receiver:
+            for t in v3vteam_emails:
+                if t[1]: # True set to receive
+                    EmailSender.send_email(text_msg, sender_address,sender_password, t[1], subject)
 
     
