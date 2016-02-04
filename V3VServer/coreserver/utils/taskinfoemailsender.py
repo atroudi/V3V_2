68e9bf9c0d66d1bc8981e2bb39ebc844505193c1
Hazem Samir <hazemhazem.1995@gmail.com>
2016-02-04 13:00:43 +0300

    refine email template

--- V3VServer/coreserver/utils/taskinfoemailsender.py
+++ V3VServer/coreserver/utils/taskinfoemailsender.py
@@ -6,22 +6,24 @@ Created on Feb 4, 2016
 
 from coreserver.utils.emailsender import EmailSender
 from coreserver.models import Email, Segment2D
+from matplotlib.testing.jpl_units.Duration import Duration
 
 class EmailsTemplates(object):
     
-    task_info_email_template = """
-        Dear %(receiver)s,
-        A video submitted by %(receiver)s to be converted to 3D video. You will receive an e-mail with the download link as soon as the video converted.
+    task_info_email_template = 
+"""
+Dear %(receiver)s,
+    We received your video to be converted to 3D video. You will receive an e-mail with the download link as soon as the video converted.
         
-        info:
-            date received: %(date)s
-            duration: %(duration)s
-            conversion task id: %(id)s
+    video info:
+        date received: %(date)s
+        duration: %(duration)s seconds
+        conversion task id: %(id)s
         
-        thanks for using our service,
+thanks for using our service,
         
-        V3V team
-    """
+V3V team
+"""
     
     
     @classmethod
@@ -31,7 +33,7 @@ class EmailsTemplates(object):
         sender_password = sender.password
         receiver = segment2D.email;
         data = {'receiver':segment2D.email, 'date':segment2D.upload_date, 'id':segment2D.id, 
-                'duration':segment2D.duration 
+                'duration':segment2D.duration
                 }
         text_msg = cls.task_info_email_template % data
         if receiver:
