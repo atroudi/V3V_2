fe3ef2792305693aa6da0fb30e1a847ab825abfc
Hazem Samir <hazemhazem.1995@gmail.com>
2016-02-08 14:34:17 +0300

    capitalize var names

--- V3VServer/V3VServer/democonfg.py
+++ V3VServer/V3VServer/democonfg.py
@@ -9,8 +9,8 @@ Created on Feb 7, 2016
 These are demo configurations and should not be used in production
 '''
 
-#list of e-mails to send debugging mails to
-v3vteam_emails = [
+# list of e-mails to send debugging mails to
+V3V_EMAILS = [
     ("hazem.s.ashmawy@gmail.com", True), 
     ("mhefeeda@qf.org.qa", False), 
     ("atroudi@qf.org.qa", False)
@@ -20,9 +20,11 @@ v3vteam_emails = [
 # this part assumes using one default resource only
 
 # field by which the instance queried from the database
-default_instance_query = dict(id=3)
+# DEFAULT_INSTANCE_QUERY = dict(ipaddress='10.2.0.9')
+DEFAULT_INSTANCE_QUERY = dict(id=3)
+
 # instance model fields used to update every time query made 
-default_instance_resource_fields = dict(
+DEFAULT_INSTANCE_FIELDS = dict(
     ipaddress = '10.2.0.9', 
     username = 'qcri\\kcalagari', 
     password = 'Qatar123',  
